474a7ef03b952bf15f90ac3f1b794962b4dc7713
Rob Gilbert <fox015@yahoo.com>
2016-03-14 20:10:59 +1000

    Added IOIO BT lib.
    Updated MainActivity to use non-compat theme.
    Imported generated material colours.

--- app/src/main/java/io/github/fox015/bikeremote/MainActivity.java
+++ app/src/main/java/io/github/fox015/bikeremote/MainActivity.java
@@ -1,13 +1,53 @@
 package io.github.fox015.bikeremote;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import ioio.lib.api.DigitalOutput;
+import ioio.lib.api.IOIO;
+import ioio.lib.api.exception.ConnectionLostException;
+import ioio.lib.util.IOIOLooper;
+import ioio.lib.util.android.IOIOActivity;
+
+public class MainActivity extends IOIOActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected IOIOLooper createIOIOLooper() {
+        return new IOIOLooper() {
+
+            public DigitalOutput mSwitchOutput;
+
+            @Override
+            public void setup(IOIO ioio) throws ConnectionLostException, InterruptedException {
+                // Force the pin to gnd to start.
+                mSwitchOutput = ioio.openDigitalOutput(1, DigitalOutput.Spec.Mode.OPEN_DRAIN, false);
+            }
+
+            @Override
+            public void loop() throws ConnectionLostException, InterruptedException {
+
+            }
+
+            @Override
+            public void disconnected() {
+
+            }
+
+            @Override
+            public void incompatible() {
+
+            }
+
+            @Override
+            public void incompatible(IOIO ioio) {
+
+            }
+        };
+    }
+
 }
